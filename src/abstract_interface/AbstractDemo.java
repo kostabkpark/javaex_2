ea3e58a223107b102f315fc38e164f12acfde1c2
kostabkpark <kostabkpark@gmail.com>
2025-02-28 19:11:52 +0900

    abstract class and interface

--- src/abstract_interface/AbstractDemo.java
+++ src/abstract_interface/AbstractDemo.java
@@ -3,6 +3,10 @@ package abstract_interface;
 public class AbstractDemo {
     public static void main(String[] args) {
         //Shape shape = new Shape();
+        Circle c = new Circle();
+        c.radius = 1;
+        c.draw();
+        System.out.println("원의 넓이는 " + c.findArea());
     }
 }
 
@@ -20,18 +24,33 @@ abstract class Shape {
 }
 
 class Circle extends Shape {
+    int radius;
     @Override
     double findArea() {
-        return 0;
+        return pi * radius * radius;
     }
 
     @Override
     void draw() {
-
+        System.out.println("원을 그리다.");
     }
 }
 
 class Rectangle extends Shape {
+    int width ;
+    int height;
+    @Override
+    double findArea() {
+        return width * height;
+    }
+
+    @Override
+    void draw() {
+        System.out.println("사각형을 그리다.");
+    }
+}
+
+class Triangle extends Shape {
     @Override
     double findArea() {
         return 0;
