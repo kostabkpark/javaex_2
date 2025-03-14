ae56e06c43e1546dd8af0edaea6387e7f97c2396
kostabkpark <kostabkpark@gmail.com>
2025-03-14 17:38:19 +0900

    stack 구현

--- src/data_structure/ListStack.java
+++ src/data_structure/ListStack.java
@@ -20,29 +20,40 @@ public class ListStack<E> {
         if(isEmpty()) {
             throw new EmptyStackException();
         }
-        return top.e;
+        return top.item;
     }
     // pop 과 push 를 구현하되 sLinkedList 의 코드를 활용해보라.
 
-    void push(E e){
+    void push(E item){
         // push 를 구현하되 sLinkedList 의 코드를 활용해보라.
         // 1단계
-
+        Node<E> newNode = new Node<>(item, top);
         // 2단계
-
+        top = newNode;
         // 3단계
-
+        size++;
     }
 
     E pop() {
         // pop 을 구현하되 sLinkedList 의 코드를 활용해보라.
-
-        return null;
+        Node<E> removedNode = top;
+        top = top.next;
+        removedNode.next = null;
+        if (size == 1) {
+            top = null;
+        }
+        size--;
+        return removedNode.item;
     }
 
     private class Node<E> {
-        E e;
+        E item;
         Node<E> next;
+
+        public Node(E newItem, Node<E> node) {
+            this.item = newItem;
+            next = node;
+        }
     }
 }
 
--- src/data_structure/ListStackDemo.java
+++ src/data_structure/ListStackDemo.java
@@ -1,7 +1,25 @@
 package data_structure;
 
+import java.util.Stack;
+
 public class ListStackDemo {
     public static void main(String[] args) {
+        Stack<String> stack = new Stack<>();
+        stack.push("A");
+        stack.push("B");
+        stack.push("C");
 
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+
+        ListStack<String> lstack = new ListStack<>();
+        lstack.push("A");
+        lstack.push("B");
+        lstack.push("C");
+
+        System.out.println(lstack.pop());
+        System.out.println(lstack.pop());
+        System.out.println(lstack.pop());
     }
 }
