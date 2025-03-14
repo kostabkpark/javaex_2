64b84795152529413fbb1ed830205fecc9f6bd6b
kostabkpark <kostabkpark@gmail.com>
2025-03-14 16:29:28 +0900

    linkedlist 구현

--- src/data_structure/SLinkedList.java
+++ src/data_structure/SLinkedList.java
@@ -9,35 +9,45 @@ public class SLinkedList<E> {
         head = tail = null;
         size = 0;
     }
-    // 탐색, 삽입, 삭제 , size 를 구하는 메서드 구현
-    int size() {
+    // 탐색, 삽입, 삭제 , size , isEmpty 를 구하는 메서드 구현
+    public int size() {
         return size;
     }
 
-    void addLast(E e){
-        Node<E> newNode = new Node<>();
-        newNode.e = e;
-        newNode.next = tail;
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    void addLast(E item){
+        Node<E> newNode = new Node<>(item, null);
+//        newNode.item = item;
+//        newNode.next = tail;
         if(size == 0) { // head == tail && head == null
             head = newNode;
         } else {
-            getNode(size - 1).next = newNode;
+            tail.next = newNode;
         }
         tail = newNode;
         size++;
     }
 
-    void addFirst(E e){
+    void addFirst(E item){
         // 1단계 : 새로운 노드를 생성한 후 node의 값을 설정한다음
         // 새로 생성된 노드가 맨 앞으로 가도록 한다.
         // < === 1) 여기를 구현한다.
+        Node<E> newNode = new Node<>(item, head);
         // 2단계 : 현재 데이터가 없는 경우, 즉 빈 리스트인 경우
         // head 와 tail 모두 newNode 가 되도록 처리한다.
         // 리스트에 데이터가 있을 경우에는 head 만 새로 추가된 노드로 변경해준다.
         // < === 2) 여기를 구현한다.
+        if(size == 0) {
+            tail = newNode;
+        }
+        head = newNode;
         // 3단계
         // 데이터가 하나 추가되었기 때문에 리스트의 사이즈를 하나 증가시킨다.
         // < === 3) 여기를 구현한다.
+        size++;
     }
 
     E getValue(int index){
@@ -45,14 +55,14 @@ public class SLinkedList<E> {
         for (int i = 0; i < index; i++) {
             find = find.next;
         }
-        return find.e;
+        return find.item;
     }
 
     Node<E> getNode(int index){
         Node<E> find = head;
-        System.out.println(find.e);
+        //System.out.println(find.item);
         for (int i = 0; i < index; i++) {
-            System.out.println(find.e);
+            //System.out.println(find.item);
             find = find.next;
         }
         return find;
@@ -60,19 +70,19 @@ public class SLinkedList<E> {
 
     E removeLast() {
         // 1단계 : 맨 마지막 노드를 지우기 전 백업받아놓는다. removedNode
-        // <=== 1) 여기를 구현한다.
+        Node<E> removedNode = tail;
         // 2-1단계 : 데이터가 2개 이상인 경우 (즉, 마지막 노드를 지워도 리스트가 비어있지 않은 경우)
-        // 지우려는 노드의 바로 이전 노드를 찾아온다.
-        // 마지막 노드를 삭제처리한다.
-        // <=== 2) 여기를 구현한다.
-        // 2-2단계 : 데이터가 1개만 있는 경우 (즉, 삭제하면 빈 리스트가 되는 경우)
-        // head 와 tail 모두 null 로 만들어준다.
-        // <=== 3) 여기를 구현한다.
+        if(size > 1) {
+            Node<E> prevNode = getNode(size - 2); // 지우려는 노드의 바로 이전 노드를 찾아온다.
+            prevNode.next = null;         // 마지막 노드를 삭제처리한다.
+            tail = prevNode;
+        } else if (size == 1) { // 2-2단계 : 데이터가 1개만 있는 경우 (즉, 삭제하면 빈 리스트가 되는 경우)
+            head = tail = null ;// 노드가 하나밖에 없는 경우, 빈 리스트로 만들기, head 와 tail 모두 null 로 만들어준다.
+        }
         // 3단계 :  노드가 하나 삭제되었기 때문에 사이즈를 하나 줄여준다.
-        // <=== 4) 여기를 구현한다.
+        size--;
         // 4단계 : 삭제된 노드(백업받아놓음)를 반환한다.
-        // <=== 5) 여기를 수정하여 완성한다.
-        return null;
+        return removedNode.item;
     }
 
     E removeFirst() {
@@ -83,11 +93,30 @@ public class SLinkedList<E> {
             head = tail = null;
         }
         size--;
-        return removedNode.e;
+        return removedNode.item;
     }
 
     private class Node<E> {
-        E e;
+        E item;
         Node<E> next;
+
+        public Node(E newItem, Node<E> node) {
+            this.item = newItem;
+            next = node;
+        }
+    }
+
+    @Override
+    public String toString() {
+        String list = "[";
+        for (Node<E> n = head; n != null ; n = n.next){
+            if(n.next != null) {
+                list += n.item + ",";
+            } else {
+                list += n.item;
+            }
+        };
+        return "SLinkedList" +
+                list + "]";
     }
 }
--- src/data_structure/SLinkedListDemo.java
+++ src/data_structure/SLinkedListDemo.java
@@ -1,9 +1,26 @@
 package data_structure;
 
+import java.util.LinkedList;
+
 public class SLinkedListDemo {
     public static void main(String[] args) {
+        LinkedList<Integer> ll = new LinkedList<>();
+        ll.addFirst(10);
+        ll.addFirst(20);
+        System.out.println(ll);
+        ll.removeFirst();
+        System.out.println(ll);
 
+        SLinkedList<Integer> sl = new SLinkedList<>();
+        sl.addFirst(10);
+        System.out.println(sl);
 
-
+        sl.addFirst(20);
+        System.out.println(sl);
+        sl.addLast(30);
+        System.out.println(sl);
+        //sl.removeFirst();
+        sl.removeLast();
+        System.out.println(sl);
     }
 }
